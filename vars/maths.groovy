859d5acbbbc99ed442760b0d0d8d2871e7faf484
chaitu2729 <109784379+chaitu2729@users.noreply.github.com>
2022-12-07 15:35:13 +0530

    Update maths.groovy

--- vars/maths.groovy
+++ vars/maths.groovy
@@ -1,17 +1,17 @@
 def sum (a,b)
 {
-   echo "sum of {a} and {b} is {a+b}
+   echo "sum of ${a} and ${b} is ${a+b}
 }
 def diff (a,b)
 {
-   echo "difference of {a} and {b} is {a-b}
+   echo "difference of ${a} and ${b} is ${a-b}
 }
 def multi (a,b)
 {
-  echo "product of {a} and {b} is {a*b}
+  echo "product of ${a} and ${b} is ${a*b}
 }
 def div (a,b)
 {
-  echo "division of {a} and {b}
+  echo "division of ${a} and ${b}  is $(a/b)
 }
 
