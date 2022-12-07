0f540a7101662b6d572a1b615385d91a3bdc4a84
chaitu2729 <109784379+chaitu2729@users.noreply.github.com>
2022-12-07 15:15:01 +0530

    Create maths.groovy

---
+++ vars/maths.groovy
@@ -0,0 +1,17 @@
+def sum (a,b)
+{
+   echo "sum of {a} and {b} is {a+b}
+}
+def sum (a,b)
+{
+   echo "difference of {a} and {b} is {a-b}
+}
+def prod (a,b)
+{
+  echo "product of {a} and {b} is {a*b}
+}
+def division (a,b)
+{
+  echo "division of {a} and {b}
+}
+
